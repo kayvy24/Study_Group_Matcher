a19a5d7e4ea96d6b2e61a6530284d132a2db46be
Kaylyn Vy <kvy@berkeley.edu>
2025-05-10 00:38:07 -0700

    surely

--- app.py
+++ app.py
@@ -408,5 +408,12 @@ def group_login_prompt():
         </form>
     '''
 
+@app.route('/logout')
+def logout():
+    session.pop('authorized_group', None)
+    session.pop('authorized_email', None)
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
